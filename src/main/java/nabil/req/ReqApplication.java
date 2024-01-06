d3681b39618db84207f3493171f6f09bd57c8e50
ahmd-nabil <ahm3dnabil99@gmail.com>
2024-01-06 17:20:44 +0200

    initial commit

---
+++ src/main/java/nabil/req/ReqApplication.java
@@ -0,0 +1,13 @@
+package nabil.req;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nabil/req/ReqApplicationTests.java
@@ -0,0 +1,13 @@
+package nabil.req;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
